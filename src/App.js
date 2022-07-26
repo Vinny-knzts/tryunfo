4a0fc6dfea18247bb09a5dbc76a93f1b8dc1e82e
Vinicius Moraes <viniciusmasouza@hotmail.com>
2022-07-25 20:04:40 -0400

    Completa requisitos 1-4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
 import React from 'react';
+import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.onInputChange = this.onInputChange.bind(this);
+
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+    };
+  }
+
+  onInputChange({ target }) {
+    const { name } = target;
+    let { value } = target;
+    if (target.type === 'checkbox') {
+      value = target.checked;
+    } else if (target.type === 'number') {
+      value = target.valueAsNumber;
+    }
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          onInputChange={ this.onInputChange }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          onInputChange={ this.onInputChange }
+        />
       </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Form extends React.Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+    return (
+      <form>
+        <label htmlFor="cardName">
+          Nome da carta:
+          <input
+            data-testid="name-input"
+            value={ cardName }
+            onChange={ onInputChange }
+            type="text"
+            name="cardName"
+            id="cardName"
+          />
+        </label>
+        <br />
+        <br />
+        <label htmlFor="cardDescription">
+          Descrição da carta:
+          <textarea
+            data-testid="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
+            name="cardDescription"
+            id="cardDescription"
+          />
+        </label>
+        <br />
+        <br />
+        <label htmlFor="cardAttr1">
+          Atributo 1:
+          <input
+            data-testid="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+            type="number"
+            name="cardAttr1"
+            id="cardAttr1"
+          />
+        </label>
+        <br />
+        <br />
+        <label htmlFor="cardAttr2">
+          Atributo 2:
+          <input
+            data-testid="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+            type="number"
+            name="cardAttr2"
+            id="cardAttr2"
+          />
+        </label>
+        <br />
+        <br />
+        <label htmlFor="cardAttr3">
+          Atributo 3:
+          <input
+            data-testid="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+            type="number"
+            name="cardAttr3"
+            id="cardAttr3"
+          />
+        </label>
+        <br />
+        <br />
+        <label htmlFor="cardImage">
+          Imagem da carta:
+          <input
+            data-testid="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
+            type="text"
+            name="cardImage"
+            id="cardImage"
+          />
+        </label>
+        <br />
+        <br />
+        <label htmlFor="cardRare">
+          Tipo da carta:
+          <select
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+            name="cardRare"
+            id="cardRare"
+          >
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+          </select>
+        </label>
+        <br />
+        <br />
+        <label htmlFor="cardTrunfo">
+          Super trunfo:
+          <input
+            data-testid="trunfo-input"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+            type="checkbox"
+            name="cardTrunfo"
+            id="cardTrunfo"
+          />
+        </label>
+        <br />
+        <br />
+        <button
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+          type="submit"
+        >
+          Salvar
+        </button>
+      </form>
+    );
+  }
+}
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.number.isRequired,
+  cardAttr2: PropTypes.number.isRequired,
+  cardAttr3: PropTypes.number.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
+
+export default Form;
